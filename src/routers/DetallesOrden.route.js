3c1170cd709897759e1890707f30f9784609e5cc
pc 2 <baquiaxfernando0@gmail.com>
2024-09-30 15:06:24 -0600

    correccion de endpoint orden id

--- src/routers/DetallesOrden.route.js
+++ src/routers/DetallesOrden.route.js
@@ -8,7 +8,7 @@ const route = Router();
 route.get('/orden/historial',validToken,authRol([client,restricted]),getHistorial);
 route.get('/orden/detalles/:idOrden',validToken,authRol([client,restricted]),getOrdenDetalles)
 route.post('/orden',validToken,authRol([client,restricted]), crearOrdenConDetalles);
-route.get('/orden/:idOrden',validToken,authRol([restricted]),getOrden);
+route.get('/orden/:idOrden',validToken,authRol([client,restricted]),getOrden);
 route.get('/orden',validToken,authRol([restricted]),getOrdenes);
 route.put('/orden/:idOrden',validToken,authRol([restricted]),updateOrden);
 
